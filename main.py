e58b36f7b7a667d7c0e53eb845acd0d6d662a71f
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-21 16:51:42 +0100

    api returns dict

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+# import pandas
 from flask import Flask, render_template
 
 app = Flask("Website")
 
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+    return render_template("home.html")
 
+
+@app.route("/api/v1/<station>/<date>")
+def api_url(station, date):
+    # df = pandas.read_csv("")
+    # temperature = df.station(date)
+    temperature = 23
+    return {"station":station,
+            "date":date,
+            "temperature":temperature}
 
 
 app.run(debug=True)
