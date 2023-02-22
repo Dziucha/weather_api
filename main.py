ed601276ecae57a863db693a16d990f89c6dcba6
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-22 23:25:15 +0100

    getting temperature per date and station using url address

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# import pandas
+import numpy as np
+import pandas
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,9 +12,16 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def api_url(station, date):
-    # df = pandas.read_csv("")
-    # temperature = df.station(date)
-    temperature = 23
+    station_number = station.zfill(6)
+    filename = f"data/TG_STAID{station_number}.txt"
+
+    df = pandas.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+
+    df["TG0"] = df["   TG"].mask(df["   TG"] == -9999, np.nan)
+    df["TG"] = df["TG0"] / 10
+
+    temperature = df.loc[df["    DATE"] == f"{date}"]["TG"].squeeze()
+
     return {"station": station,
             "date": date,
             "temperature": temperature}
